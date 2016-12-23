dee1f3585fbe69bed3f0c06e4e040fd3fef2dc50
anoopda <anoopda@qburst.com>
2016-12-23 15:43:34 +0530

    qr code detected

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
+import QrCode from 'qrcode-reader';
+var qr = new QrCode();
+
+
 $(document).ready(function() {
-	
+		
+
 	var input = document.querySelector('input[type=file]');
 	input.addEventListener('change', function () {
     	var file = input.files[0];
@@ -7,14 +12,28 @@ $(document).ready(function() {
 	});
 
 	function drawOnCanvas(file) {
+
+		 qr.callback = function(result,err) {
+		 	console.log(result);
+		 	$('#imageContainer').append(result);	
+		 }
+
+
+
+
+
+
 		// We use the File APi to read the content of the input file element
 	   var reader = new FileReader();
 
 	    // On reader load with parse the content and draw it on Canvas
 	    reader.onload = function (e) {
-	        var dataURL = e.target.result;
-	        /*$('#imageContainer').append('<img src = '+ dataURL +' id ="imageId">');*/
+	       var dataURL = e.target.result;
 	        $('#tempImage').attr("src", dataURL);
+
+
+	         var data =	qr.decode(dataURL);
+	      
 	    };
 
 	    reader.readAsDataURL(file);
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    entry: "./js/script.js",
+    output: {
+        path: __dirname + '/build/',
+        filename: "bundle.js"
+    },
+
+ module: {
+        loaders: [
+            {
+               test: /\.js$/,
+               exclude: /node_modules/,
+			   loader: 'babel-loader',
+			    query: {
+                     presets: ['es2015']
+                 }         
+
+    		}
+        ]
+    }
+
+   
+};
